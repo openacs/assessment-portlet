c969e68cb22ea25e12a803cb55c4e30543596030
Hector Romojaro <hector.romojaro@gmail.com>
2020-08-21 17:42:50 +0000

    Add 'assessment_admin_portlet::get_pretty_name' and 'assessment_portlet::get_pretty_name' to 'assessment_portlet_links_names' test case

--- tcl/test/assessment-portlet-procs.tcl
+++ tcl/test/assessment-portlet-procs.tcl
@@ -9,14 +9,18 @@ ad_library {
 aa_register_case -procs {
         assessment_admin_portlet::link
         assessment_portlet::link
+        assessment_admin_portlet::get_pretty_name
+        assessment_portlet::get_pretty_name
     } -cats {
         api
         production_safe
-    } assessment_portlet_links {
-        Test diverse link procs.
+    } assessment_portlet_links_names {
+        Test diverse link and name procs.
 } {
-    aa_equals "Assessment admin portlet link" "[assessment_admin_portlet::link]" ""
-    aa_equals "Assessment portlet link"       "[assessment_portlet::link]" ""
+    aa_equals "Assessment admin portlet link"           "[assessment_admin_portlet::link]" ""
+    aa_equals "Assessment portlet link"                 "[assessment_portlet::link]" ""
+    aa_equals "Assessment admin portlet pretty name"    "[assessment_admin_portlet::get_pretty_name]" "#assessment.Assessment_Administration#"
+    aa_equals "Assessment portlet pretty name"          "[assessment_portlet::get_pretty_name]" "#assessment.Assessment#"
 }
 
 # Local variables:
